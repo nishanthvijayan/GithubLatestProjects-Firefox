4495098d95fee70587f0ad3fafead82dd2ce29c5
Nishanth Vijayan <nishanththegr8@gmail.com>
2015-05-03 06:10:16 +0530

    Added loading,info icons and their functions.Fixed some minor errors

--- firefox/data/popup.js
+++ firefox/data/popup.js
@@ -22,12 +22,13 @@ function putdata(json)
 }
 
 function fetchdata(){
+  imgToggle();
   date = new Date(new Date().getTime()-7*24*60*60*1000).toISOString().slice(0,10)
   req =  new XMLHttpRequest();
   req.open("GET",'https://api.github.com/search/repositories?q=created:%3E'+date+'&sort=stars&order=desc',true);
   req.send();
   req.onload = function(){
-
+    imgToggle();
     $("span").remove();
     $("footer a:nth-child(2)").before('<span>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<iframe src="https://ghbtns.com/github-btn.html?user=nishanthvijayan&repo=GithubLatestProjects-Firefox&type=star&count=true" frameborder="0" scrolling="0" width="100px" height="20px"></iframe></span>');
     
@@ -38,10 +39,17 @@ function fetchdata(){
     putdata(res);
 
   };
+  req.onerror = function(){
+    imgToggle();
+  }
 
 }
 
-
+function imgToggle(){
+  src = $('.loading').attr('src');
+  if(src=="img/refresh-white.png") $(".loading").attr("src","img/ajax-loader.gif");
+  else $(".loading").attr("src","img/refresh-white.png");
+}
 
 $(document).ready(function(){
 
@@ -68,5 +76,14 @@ $(document).ready(function(){
     return false;
   });
 
+  $("body").on('click',".info", function(){
+    window.alert("The Github projects show here were all created within the past 7 days and are ordered according to the number of star they have.");
+    return false;
+  });
+
+  $("body").on('click',".loading", function(){
+    fetchdata();
+    return false;
+  });
 });
 
